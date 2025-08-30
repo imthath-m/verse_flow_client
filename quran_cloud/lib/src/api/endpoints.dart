afbb31e975e278068246513fc0a24d900d577190
Mohammed Imthathullah <imthath.m@hotmail.com>
2025-08-30 23:09:07 +0530

    more removal. tests failing.

--- quran_cloud/lib/src/api/endpoints.dart
+++ quran_cloud/lib/src/api/endpoints.dart
@@ -3,99 +3,82 @@ import '../utils/constants.dart';
 /// API endpoints for Al-Quran Cloud API
 class ApiEndpoints {
   static const String _baseUrl = QuranCloudConstants.baseUrl;
-  
+
   /// Get list of all surahs
   static String get surahs => '$_baseUrl/surah';
-  
+
   /// Get specific surah by number
   static String surah(int number) => '$_baseUrl/surah/$number';
-  
+
   /// Get specific ayah by location
   static String ayah(String location) => '$_baseUrl/ayah/$location';
-  
+
   /// Get specific ayah by surah and ayah numbers
-  static String ayahByNumbers(int surah, int ayah) => '$_baseUrl/ayah/$surah:$ayah';
-  
+  static String ayahByNumbers(int surah, int ayah) =>
+      '$_baseUrl/ayah/$surah:$ayah';
+
   /// Get list of all editions
   static String get editions => '$_baseUrl/edition';
-  
+
   /// Get editions by format and type
-  static String editionsByFormat(String format, {String? type, String? language}) {
+  static String editionsByFormat(
+    String format, {
+    String? type,
+    String? language,
+  }) {
     final params = <String>['format=$format'];
     if (type != null) params.add('type=$type');
     if (language != null) params.add('language=$language');
     return '$_baseUrl/edition?${params.join('&')}';
   }
-  
+
   /// Get audio editions (recitations)
   static String audioEditions({String? language}) {
     return editionsByFormat('audio', type: 'versebyverse', language: language);
   }
-  
+
   /// Get translation editions
   static String translationEditions() {
     return editionsByFormat('text', type: 'translation');
   }
-  
+
   /// Get editions for specific language
   static String editionsByLanguage(String language) {
     return '$_baseUrl/edition?language=$language';
   }
-  
+
   /// Get surah with specific edition
   static String surahWithEdition(int surahNumber, String edition) {
     return '$_baseUrl/surah/$surahNumber/$edition';
   }
-  
+
   /// Get ayah with specific edition
   static String ayahWithEdition(String location, String edition) {
     return '$_baseUrl/ayah/$location/$edition';
   }
-  
+
   /// Get ayah with specific edition by numbers
   static String ayahWithEditionByNumbers(int surah, int ayah, String edition) {
     return '$_baseUrl/ayah/$surah:$ayah/$edition';
   }
-  
+
   /// Get languages list
   static String get languages => '$_baseUrl/edition/language';
-  
+
   /// Get juz by number
   static String juz(int number) => '$_baseUrl/juz/$number';
-  
+
   /// Get juz with specific edition
-  static String juzWithEdition(int number, String edition) => '$_baseUrl/juz/$number/$edition';
-  
+  static String juzWithEdition(int number, String edition) =>
+      '$_baseUrl/juz/$number/$edition';
+
   /// Get page by number
   static String page(int number) => '$_baseUrl/page/$number';
-  
+
   /// Get page with specific edition
-  static String pageWithEdition(int number, String edition) => '$_baseUrl/page/$number/$edition';
-  
-  /// Get hizb by number
-  static String hizb(int number) => '$_baseUrl/hizb/$number';
-  
-  /// Get hizb with specific edition
-  static String hizbWithEdition(int number, String edition) => '$_baseUrl/hizb/$number/$edition';
-  
-  /// Get ruku by number
-  static String ruku(int number) => '$_baseUrl/ruku/$number';
-  
-  /// Get ruku with specific edition
-  static String rukuWithEdition(int number, String edition) => '$_baseUrl/ruku/$number/$edition';
-  
-  /// Get manzil by number
-  static String manzil(int number) => '$_baseUrl/manzil/$number';
-  
-  /// Get manzil with specific edition
-  static String manzilWithEdition(int number, String edition) => '$_baseUrl/manzil/$number/$edition';
-  
-  /// Get sajda ayahs
-  static String get sajda => '$_baseUrl/sajda';
-  
-  /// Get sajda ayahs with specific edition
-  static String sajdaWithEdition(String edition) => '$_baseUrl/sajda/$edition';
-  
+  static String pageWithEdition(int number, String edition) =>
+      '$_baseUrl/page/$number/$edition';
+
   /// Search in Quran text
   static String search(String query, {String? edition, int? page, int? size}) {
     final params = <String>['q=$query'];
@@ -104,10 +87,11 @@ class ApiEndpoints {
     if (size != null) params.add('size=$size');
     return '$_baseUrl/search/${Uri.encodeComponent(query)}?${params.join('&')}';
   }
-  
+
   /// Get quran info
   static String get quranInfo => '$_baseUrl/quran';
-  
+
   /// Get quran info with specific edition
-  static String quranInfoWithEdition(String edition) => '$_baseUrl/quran/$edition';
+  static String quranInfoWithEdition(String edition) =>
+      '$_baseUrl/quran/$edition';
 }
--- quran_cloud/lib/src/models/surah.dart
+++ quran_cloud/lib/src/models/surah.dart
@@ -3,129 +3,38 @@ import 'ayah.dart';
 
 part 'surah.g.dart';
 
-/// Represents a surah (chapter) of the Quran
 @JsonSerializable()
 class Surah {
-  /// Surah number (1-114)
   final int number;
-
-  /// Arabic name of the surah
   final String name;
-
-  /// English name of the surah
   final String englishName;
-
-  /// English translation of the name
   final String englishNameTranslation;
-
-  /// Number of ayahs in the surah
   final int numberOfAyahs;
-
-  /// Type of revelation (Meccan or Medinan)
   final String revelationType;
 
-  /// List of ayahs in the surah (optional, may be null if not loaded)
-  final List<Ayah>? ayahs;
-
-  const Surah({
+  Surah({
     required this.number,
     required this.name,
     required this.englishName,
     required this.englishNameTranslation,
     required this.numberOfAyahs,
     required this.revelationType,
-    this.ayahs,
   });
 
-  /// Creates a copy of this surah with updated values
-  Surah copyWith({
-    int? number,
-    String? name,
-    String? englishName,
-    String? englishNameTranslation,
-    int? numberOfAyahs,
-    String? revelationType,
-    List<Ayah>? ayahs,
-  }) {
-    return Surah(
-      number: number ?? this.number,
-      name: name ?? this.name,
-      englishName: englishName ?? this.englishName,
-      englishNameTranslation:
-          englishNameTranslation ?? this.englishNameTranslation,
-      numberOfAyahs: numberOfAyahs ?? this.numberOfAyahs,
-      revelationType: revelationType ?? this.revelationType,
-      ayahs: ayahs ?? this.ayahs,
-    );
+  factory Surah.fromJson(Map<String, dynamic> json) => _$SurahFromJson(json);
+  Map<String, dynamic> toJson() => _$SurahToJson(this);
+
+  @override
+  String toString() {
+    return 'Surah $number. $englishName';
   }
 
-  /// Checks if this surah has ayahs loaded
-  bool get hasAyahs => ayahs != null && ayahs!.isNotEmpty;
-
-  /// Gets the number of loaded ayahs
-  int get loadedAyahsCount => ayahs?.length ?? 0;
-
-  /// Checks if this is a Meccan surah
-  bool get isMeccan => revelationType.toLowerCase() == 'meccan';
-
-  /// Checks if this is a Medinan surah
-  bool get isMedinan => revelationType.toLowerCase() == 'medinan';
-
-  /// Gets a specific ayah by its number in the surah
-  Ayah? getAyah(int ayahNumber) {
-    if (!hasAyahs) return null;
-    try {
-      return ayahs!.firstWhere((ayah) => ayah.numberInSurah == ayahNumber);
-    } catch (e) {
-      return null;
-    }
-  }
-
-  /// Gets a range of ayahs
-  List<Ayah> getAyahsRange(int start, int end) {
-    if (!hasAyahs) return [];
-    if (start < 1 || end > numberOfAyahs || start > end) return [];
-
-    return ayahs!
-        .where(
-          (ayah) => ayah.numberInSurah >= start && ayah.numberInSurah <= end,
-        )
-        .toList();
-  }
-
-  /// Equality operator
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
-    return other is Surah &&
-        other.number == number &&
-        other.name == name &&
-        other.englishName == englishName &&
-        other.englishNameTranslation == englishNameTranslation &&
-        other.numberOfAyahs == numberOfAyahs &&
-        other.revelationType == revelationType &&
-        other.ayahs == ayahs;
+    return other is Surah && other.number == number;
   }
 
-  /// Hash code
   @override
-  int get hashCode {
-    return number.hashCode ^
-        name.hashCode ^
-        englishName.hashCode ^
-        englishNameTranslation.hashCode ^
-        numberOfAyahs.hashCode ^
-        revelationType.hashCode ^
-        ayahs.hashCode;
-  }
-
-  /// String representation
-  @override
-  String toString() {
-    return 'Surah(number: $number, name: $name, ayahs: $numberOfAyahs, loaded: $loadedAyahsCount)';
-  }
-
-  /// JSON serialization
-  factory Surah.fromJson(Map<String, dynamic> json) => _$SurahFromJson(json);
-  Map<String, dynamic> toJson() => _$SurahToJson(this);
+  int get hashCode => number.hashCode;
 }
